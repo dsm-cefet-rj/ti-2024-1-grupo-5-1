e59de870d3b9516f5add6fcf8e109fb688a1d30a
K0R0B0R0S <joseasfer07@gmail.com>
2024-03-30 18:13:55 -0300

    add: react-router-dom e Router component

--- frontend/chapa-quente/src/App.jsx
+++ frontend/chapa-quente/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Produtos from './components/Produtos'
+import Router from './components/Router'
 
 import "./App.css"
 
---
+++ frontend/chapa-quente/src/components/Router/index.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export default Router = () => {
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    {/* Adicione aqui as rotas Ex: <Route path='produtos' element={<Produtos/>} />*/}
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
+}
