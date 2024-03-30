53bb94e36769fc851b939e48adc9511a4b22d9c6
K0R0B0R0S <joseasfer07@gmail.com>
2024-03-30 19:10:19 -0300

    update: adicionando Produtos em Router

--- frontend/chapa-quente/src/components/Router/index.jsx
+++ frontend/chapa-quente/src/components/Router/index.jsx
@@ -1,13 +1,40 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useState } from 'react';
+import Produtos from '../Produtos'
+import Pedidos from '../Pedido'
 
-export default Router = () => {
+const Router = () => {
+    const [carrinho, setCarrinho] = useState([])
+    const [quantity, setQuantity] = useState(0)
+
+    function handleAddCarrinho(item){
+        setCarrinho([...carrinho, item])
+        setQuantity(quantity+1)
+    }
+
+    function handleRemoveCarrinho(item) {
+        if (carrinho.length > 0){
+            var newCarrinho = carrinho
+            const indexs = carrinho.map((itemCarrinho) => itemCarrinho.id)
+            const indexToRemove = indexs.lastIndexOf(item.id)
+            newCarrinho.splice(indexToRemove, 1)
+            setCarrinho(newCarrinho)
+            setQuantity(quantity-1)
+        }
+    }
+
     return (
         <>
             <BrowserRouter>
                 <Routes>
                     {/* Adicione aqui as rotas Ex: <Route path='produtos' element={<Produtos/>} />*/}
+                    <Route path='/produtos' element={
+                        <Produtos quantity={quantity} handleAddCarrinho = {handleAddCarrinho} handleRemoveCarrinho = {handleRemoveCarrinho}/>} />
+                    <Route path='/pedidos' element={<Pedidos/>} />
                 </Routes>
             </BrowserRouter>
         </>
     )
 }
+
+export default Router;
