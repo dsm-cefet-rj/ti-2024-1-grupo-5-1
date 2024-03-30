db0aa063dcb71e1e938159f0e00d3c00d31bbab3
K0R0B0R0S <joseasfer07@gmail.com>
2024-03-30 19:12:41 -0300

    update: removendo carrinho de App.jsx e adicionando em Router

--- frontend/chapa-quente/src/App.jsx
+++ frontend/chapa-quente/src/App.jsx
@@ -6,25 +6,6 @@ import Router from './components/Router'
 import "./App.css"
 
 const App = () => {
-    const [carrinho, setCarrinho] = useState([])
-    const [quantity, setQuantity] = useState(0)
-
-    function handleAddCarrinho(item){
-        setCarrinho([...carrinho, item])
-        setQuantity(quantity+1)
-    }
-
-    function handleRemoveCarrinho(item) {
-        if (carrinho.length > 0){
-            var newCarrinho = carrinho
-            const indexs = carrinho.map((itemCarrinho) => itemCarrinho.id)
-            const indexToRemove = indexs.lastIndexOf(item.id)
-            newCarrinho.splice(indexToRemove, 1)
-            setCarrinho(newCarrinho)
-            setQuantity(quantity-1)
-        }
-    }
-
     return (
         <>
             <div className='App'>
